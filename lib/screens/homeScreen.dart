63a106f7ef2a6421cd520acb3354784a00cfa4bd
mordvash <mordvash1@gmail.com>
2021-08-31 08:04:13 +0300

    Adding title and icons

--- lib/screens/homeScreen.dart
+++ lib/screens/homeScreen.dart
@@ -9,11 +9,49 @@ class HomeScreen extends StatelessWidget {
       body: Container(
         decoration: BoxDecoration(
           image: DecorationImage(
-              image: AssetImage("images/homePic.jpeg"),
-            fit: BoxFit.cover
-          )
+            image: AssetImage("images/homePic.jpeg"),
+            fit: BoxFit.cover,
+          ),
         ),
-
+        child: SafeArea(
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text.rich(
+                  TextSpan(
+                      text: 'Fit',
+                      style: TextStyle(fontSize: 80),
+                      children: [
+                        TextSpan(
+                          text: 'Fix',
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold, fontSize: 80),
+                        ),
+                      ]),
+                ),
+                SizedBox(height: 20),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    IconButton(
+                      onPressed: () {},
+                      icon: Icon(Icons.email),
+                    ),
+                    IconButton(
+                      onPressed: () {},
+                      icon: Icon(Icons.email),
+                    ),
+                    IconButton(
+                      onPressed: () {},
+                      icon: Icon(Icons.email),
+                    )
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
