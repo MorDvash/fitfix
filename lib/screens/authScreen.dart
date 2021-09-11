5a8d1746d98ea280fb76e5c5aec25351335d8445
mordvash <mordvash1@gmail.com>
2021-09-11 19:56:40 +0300

    Adding password visible

--- lib/screens/authScreen.dart
+++ lib/screens/authScreen.dart
@@ -114,14 +114,31 @@ class TextForm extends StatefulWidget {
   _TextFormState createState() => _TextFormState();
 }
 
+@override
 class _TextFormState extends State<TextForm> {
+  var _passwordVisible = true;
   @override
   Widget build(BuildContext context) {
     return TextFormField(
-      obscureText: widget.hideText,
+      obscureText: _passwordVisible,
       decoration: InputDecoration(
         labelText: widget.placeholder,
         border: OutlineInputBorder(),
+        suffixIcon: IconButton(
+          icon: Icon(
+            widget.hideText
+                ? _passwordVisible
+                    ? Icons.visibility
+                    : Icons.visibility_off
+                : null,
+            color: Theme.of(context).primaryColorDark,
+          ),
+          onPressed: () {
+            setState(() {
+              _passwordVisible = !_passwordVisible;
+            });
+          },
+        ),
       ),
       onSaved: (value) => widget.onSaved(value, widget.variable),
       validator: widget.validator,
