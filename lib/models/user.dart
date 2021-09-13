72bd3fbf8d45eafbb75070c946f87f879cb88f4b
mordvash <mordvash1@gmail.com>
2021-09-13 15:35:43 +0300

    adding User model

--- lib/models/user.dart
+++ lib/models/user.dart
@@ -1,1 +1,12 @@
-class User {}
+class User {
+  final String userName;
+  final String email;
+  final String token;
+  final String imageUrl;
+
+  User(
+      {required this.userName,
+      required this.email,
+      required this.token,
+      required this.imageUrl});
+}
