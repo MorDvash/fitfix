8644eb554cc5061b32e958935a0810259ea927d9
mordvash <mordvash1@gmail.com>
2021-10-02 10:42:01 +0300

    showing demo data

--- lib/screens/fitnessInstructors.dart
+++ lib/screens/fitnessInstructors.dart
@@ -1,12 +1,41 @@
+import 'package:fitfix/provider/instructorsProvider.dart';
 import 'package:fitfix/widgets/drawer.dart';
+import 'package:fitfix/widgets/instructorCard.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
-class FitnessInstructorsScreen extends StatelessWidget {
+class FitnessInstructorsScreen extends StatefulWidget {
   static const routeName = 'fitnessInstructorsScreen';
   const FitnessInstructorsScreen({Key? key}) : super(key: key);
 
   @override
+  _FitnessInstructorsScreenState createState() =>
+      _FitnessInstructorsScreenState();
+}
+
+class _FitnessInstructorsScreenState extends State<FitnessInstructorsScreen> {
+  var _isInit = true;
+  var _isLoading = false;
+
+  @override
+  void didChangeDependencies() {
+    if (_isInit) {
+      setState(() {
+        _isLoading = true;
+      });
+      Provider.of<InstructorsProvider>(context).getInstructorsData();
+      setState(() {
+        _isLoading = false;
+      });
+    }
+    _isInit = false;
+    super.didChangeDependencies();
+  }
+
+  @override
   Widget build(BuildContext context) {
+    final instructorList =
+        Provider.of<InstructorsProvider>(context).instructors;
     return SafeArea(
       child: Scaffold(
         appBar: AppBar(
@@ -36,6 +65,13 @@ class FitnessInstructorsScreen extends StatelessWidget {
                     label: Text('סינון')),
               ],
             ),
+            ListView.builder(
+                scrollDirection: Axis.vertical,
+                shrinkWrap: true,
+                itemCount: instructorList.length,
+                itemBuilder: (context, index) {
+                  return InstructorCard();
+                }),
           ],
         ),
       ),
